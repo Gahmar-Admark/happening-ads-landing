07428dbf6799917c7b61e8186380d2b2d4299bd8
Rohit Sharma <rohitsharma2k3@gmail.com>
2025-09-01 16:11:38 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-
 'use client';
 import AboutBanner from "./home/banner";
 import Bloglisting from "./blogs/blogs";
+// import htmlFile from '../../public/index.html';
 
 export default function Home() {
-  return (<>
-  <AboutBanner/>
-    </>
-    
+  return (
+    <iframe
+      src="/index.html"
+      style={{ width: "100%", height: "118vh", border: "none"  }}
+    />
   );
 }
