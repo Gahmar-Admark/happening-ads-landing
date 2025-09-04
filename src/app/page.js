6840f2064839398c3b8425977d68c09aeed3b129
Rohit Sharma <rohitsharma2k3@gmail.com>
2025-09-04 12:23:31 +0530

    Replace iframe with video element for improved media presentation

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
-'use client';
+"use client";
 import AboutBanner from "./home/banner";
 import Bloglisting from "./blogs/blogs";
 // import htmlFile from '../../public/index.html';
 
 export default function Home() {
   return (
-    <iframe
-      src="/index.html"
-      style={{ width: "100%", height: "118vh", border: "none"  }}
+    <video
+      src="https://res.cloudinary.com/dvqzu9oin/video/upload/v1756968731/gahmar/videoplayback_sjnxoo.mp4"
+      autoPlay
+      loop
+      muted
+      controls={false}
+      style={{ width: '100%', height: '100vh', objectFit: 'cover', margin: 0 }}
     />
   );
 }
